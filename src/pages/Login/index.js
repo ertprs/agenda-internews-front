9778ed22c465d7d14289140a740283ac112699b6
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2020-11-28 15:52:24 -0300

    initialized app

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Login = () => {
+    return(
+        <S.Container>
+            <h1>Login</h1>
+        </S.Container>
+    )
+}
+
+export default Login
